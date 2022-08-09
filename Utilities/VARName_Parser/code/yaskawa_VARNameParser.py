7c32899c2bc1f56a89b86c1f5099765fa065eaa1
noveon_aking <aking@noveon.co>
2022-08-09 14:17:01 -0500

    Updates from Daniel Luna, changes to credits to include him in the code development

--- Utilities/VARName_Parser/code/yaskawa_VARNameParser.py
+++ Utilities/VARName_Parser/code/yaskawa_VARNameParser.py
@@ -10,26 +10,32 @@ from collections import OrderedDict
 
 # Owned
 __author__ = "Rebecca Iglesias-Flores"
-__copyright__ = "Copyright 2021, Revecca Iglesias-Flores"
-__credits__ = ["Rebecca Iglesias-Flores"]
+__copyright__ = "Copyright 2021,  "
+__credits__ = ["Rebecca Iglesias-Flores",
+               "Daniel Luna",
+               "Andrew King"]
 __license__ = "MIT"
 __version__ = "0.1.0"
-__maintainer__ = "Andrew King"
-__email__ = ["irebecca@seas.upenn.edu","acking@utexas.edu"]
+__maintainer__ = ["Rebecca Iglesias-Flores",
+                  "Daniel Luna",
+                  "Andrew King"]
+__email__ = ["dluna@noveon.co",
+             "aking@noveon.co",
+             "acking1187@gmail.com"]
 __status__ = "Dev"
 
-"""Description of module: yaskawa_VARNameParser module reads input .xlsx tracker file from a VARNAME CDS sheet (in data/ folder) and outputs .DAT file that's in the format a DX200 robot controller needs."""
+"""Description of module: yaskawa_parser module reads input .xlsx tracker file from data/ folder and outputs .DAT file that's in the format the robot controller needs."""
 
 """Usage:
-Source Folder: E:\GitRepo\PersonalProjects\Yaskawa-Motoman\Utilities\VARName_Parser\
+Source Folder: /Users/riglesias-flores/Documents/Noveon_Development/Betatest/
 Select a Source Folder of your own choosing (mine is shown above^) and ensure 
 there is a 'code/' folder with the yaskawa_parser.py script in it and 
 a 'data/' folder with your excel input file.  Then run command line below from
 terminal from your Source Folder.
 
 
-python -u code/yaskawa_VARNameparser.py \
---var_tracker_fname CDSFileName.xlsx \
+python -u code/yaskawa_VARNameParser.py \
+--var_tracker_fname 792164-1-1_CDS_VAR_Rev.0.xlsx \
 --dataDir data/ \
 --outDir out/ \
 --outSubdir text_parser_project/ \
@@ -46,7 +52,8 @@ header_dict = OrderedDict({
                                 'R VAR':'///R',
                                 'S VAR':'///S',
                                 'P VAR':'///P',
-                                'TM VAR':'///BP'
+                                'TM VAR':'///BP',
+                                'EX VAR':'///EX'
                                 })
 print(header_dict.keys())
 
@@ -68,7 +75,7 @@ def register_arguments():
     parser.add_argument(
         '--var_tracker_fname', type=str,
         required=True,
-        help="Filename of the excel variable tracker file. ex. 'CDSFileName.xlsx'"
+        help="Filename of the excel variable tracker file. ex. '_792164-1-1_CDS_VAR_Rev.0.xlsx'"
         )
     parser.add_argument(
         '--dataDir', type=str,
@@ -165,7 +172,7 @@ def confirm_directories():
 def pad_data(DAT_dict):
 
     # pad with carriage returns
-    padded_lengths = [3,2004,4004,6003,8004,10004,20004] + [0]
+    padded_lengths = [3,2004,4005,6006,8007,10008,20009,30010] + [40011]
 
     for section_no, (section_title, data) in enumerate(DAT_dict.items()):
         curr_len = len(data) + 1 # plus 1 to account for section header taking up a line          
@@ -176,14 +183,15 @@ def pad_data(DAT_dict):
         ncarriage_returns = ['\r\n']*(actual_padding_remaining)
         data.extend(ncarriage_returns)
 
+
 def write_to_DAT_file(DAT_dict, lengths, outfile):
 
 
     with open(outfile, 'w') as fout:
 
         # write static header
-        fout.write('//VARNAME\r\n')
-        fout.write('//SHARE 2000,2000,2000,2000,2000,10000,10000,10000\r\n')
+        fout.write('//VARNAME\n')
+        fout.write('///SHARE 2000,2000,2000,2000,2000,10000,10000,10000\n')
         
         # write data
         for sheet_name, data in DAT_dict.items():
@@ -191,19 +199,21 @@ def write_to_DAT_file(DAT_dict, lengths, outfile):
             # write section header
             section_title = header_dict[sheet_name]
 
-            fout.write(section_title + '\r\n')
+            fout.write(section_title + '\n') 
 
             # section data
             for line_no, item in enumerate(data):
 
                 if item == '\r\n':
                     # write padded carriage returns
-                    fout.write('\r\n')
+                    fout.write('\n')
+                elif str(item[1]) == 'nan':
+                    fout.write('\n')
                 else:
                     # write section data
                     # <class 'str'>, <class 'str'>
                     str_int, name = item[0], item[1]
-                    fout.write(str_int + " 1,0," + name + '\r\n')
+                    fout.write(str_int + " 1,0," + name + '\n')
 
 
 #########################
@@ -233,18 +243,14 @@ def run():
         # <class 'pandas.core.frame.DataFrame'>
         df = excel_workbook_dict[sheet_name]
 
-        # data always starts at index 7
+        # data always starts at index 8
         # <class 'pandas.core.frame.DataFrame'>
-        selected_rows = df.iloc[7:,]
+        selected_rows = df.iloc[8:,]
         print(f'confirm selected rows for sheet {sheet_name}')
         print(selected_rows.head())
 
-        # Select rows which do not have NaN value in column 1 ('Unnamed: 1') of excel sheet
-        # Columns: [CUSTOMER:, Unnamed: 1, User_Name, Unnamed: 3, Unnamed: 4]
-        filtered_df = selected_rows[~selected_rows['Unnamed: 1'].isnull()]
-
         # select the hashtag column and the Name column
-        temp_hashtag_col, name_col = filtered_df.iloc[:,0].map(str).to_list(), filtered_df.iloc[:,1].to_list()
+        temp_hashtag_col, name_col = selected_rows.iloc[:,0].map(str).to_list(), selected_rows.iloc[:,1].to_list()
         # pad zeros
         hashtag_col = [num.zfill(4) for num in temp_hashtag_col]
 
@@ -262,7 +268,7 @@ def run():
 
     # dynamically pad compiled data
     pad_data(DAT_dict)
-    
+
     # write outfile
     write_to_DAT_file(DAT_dict, DAT_lengths, DAT_outfile)
 
